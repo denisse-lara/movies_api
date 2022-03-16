98d7e1ca1ca00d8d26e9d721b2a35a19b5b12dd7
Denisse Lara <denisse.lara@goluuk.com>
2022-03-15 21:09:54 -0400

    chore(db): drop migrations and redo
    in postgres, dropped the alembic_version table
    in flask shell, dropped db
    delete migration folder
    
    commands:
    flask db init
    flask db migrate -m "user and jwt models"
    flask db upgrade

--- api/model/auth.py
+++ api/model/auth.py
@@ -2,9 +2,11 @@ from app import db
 
 
 class JWTWhitelist(db.Model):
+    __tablename__ = "jwt_whitelist"
+
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, nullable=False, unique=True)
-    token = db.Column(db.String(100), nullable=False, unique=True)
+    token = db.Column(db.String(255), nullable=False, unique=True)
 
     def __str__(self):
         return f"user: '{self.user_id}'\ttoken: {self.token}"
